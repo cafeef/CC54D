a8686d63c8041dce59cb3f7cfdea05178d028e98
Fernanda Pacheco <109240203+cafeef@users.noreply.github.com>
2025-08-12 11:29:03 -0300

    alterei o exemplo de saida de dados

--- src/aula01/SaidaDados.java
+++ src/aula01/SaidaDados.java
@@ -2,7 +2,13 @@ package aula01;
 
 public class SaidaDados {
 	public static void main(String [] args) {
-		System.out.print("Universidade Tecnógica Federal do Paraná");
-		System.out.println("Programação Orientada a Objetos");
+		
+		String nome = "João da Silva";
+		int idade = 30;
+		double peso = 78.9;
+		
+		System.out.println("Universidade Tecnógica Federal do Paraná");
+		System.out.println(nome + " possui " + idade + " anos e " + String.format("%.2f", peso) + "kg.");
+		System.out.printf("%s possui %d e %.2f kg.\n", nome, idade, peso);
 	}
 }
