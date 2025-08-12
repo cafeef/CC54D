1bfc30d508a1f093b444227a33dbd4de3ef44a81
Fernanda Pacheco <109240203+cafeef@users.noreply.github.com>
2025-08-12 11:16:25 -0300

    Add SaidaDados example for data output
    Introduces SaidaDados.java, demonstrating basic console output in Java for the 'Programação Orientada a Objetos' course.

---
+++ src/aula01/SaidaDados.java
@@ -0,0 +1,8 @@
+package aula01;
+
+public class SaidaDados {
+	public static void main(String [] args) {
+		System.out.print("Universidade Tecnógica Federal do Paraná");
+		System.out.println("Programação Orientada a Objetos");
+	}
+}
