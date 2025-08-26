058f30a9bf788dc899e80b0a42ec87f332df5705
Fernanda Pacheco <cafeef16@gmail.com>
2025-08-26 11:06:35 -0300

    Add Carro, Pessoa classes and CarroTeste
    Introduces the Carro and Pessoa classes with basic attributes and methods. Adds CarroTeste to demonstrate object instantiation and usage of Carro methods.

---
+++ src/aula05/Pessoa.java
@@ -0,0 +1,21 @@
+package aula05;
+
+public class Pessoa {
+	//Atributos da classe Pessoa
+	String nome;
+	int idade;
+	double peso;
+	
+	//métodos da classe pessoa
+	public void andar() {
+		
+	}
+	
+	public void correr() {
+		
+	}
+	
+	public void falar() {
+		
+	}
+}
